b87b3049d86b2b4f0cb8c2d8c20cbe992636d446
Jordan_M_Wofford <jmwofford@gmail.com>
2019-01-24 15:34:18 -0600

    created new Branch. LOCKTHISREPO

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -1,5 +1,25 @@
 class ShoesController < ApplicationController
 
-  def index
+  before_action :authenticate
+
+  def index 
+    
+  end
+
+  def create 
+  #  @perform = VmRequest.perform 
+  end
+
+  def update
+    # @perform = VmRequest.perform 
+    redirect_to '/dashboard'
+  end
+  private 
+
+  def authenticate
+    current_user 
+    unless @current_user
+      redirect_to '/'
+    end
   end
 end
